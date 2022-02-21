cb6689082100f411e1cdee61231d2cb1d54b04ba
Manan <mananrastogi1226@gmail.com>
2022-02-21 15:51:02 +0530

    Adding functionality

---
+++ Project 10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project 10
+//
+//  Created by Daffolapmac-155 on 21/02/22.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    init(name: String, image: String){
+        self.name = name
+        self.image = image
+    }
+
+}
---
+++ Project 10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  Project 10
+//
+//  Created by Daffolapmac-155 on 21/02/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
